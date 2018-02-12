c1bcdc6af268bc59c5c82ddd93460669cf81421e
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-02-12 14:11:28 +0100

    Interface studentlist

--- stdWCF/Service1.svc.cs
+++ stdWCF/Service1.svc.cs
@@ -12,22 +12,18 @@ namespace stdWCF
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        static List<Student> students = new List<Student>();
+
+        public void AddStudents(string Name, string Klasse, string Rum)
         {
-            return string.Format("You entered: {0}", value);
+            Student std = new Student(Name, Klasse, Rum);
+            students.Add(std);
+            
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public List<Student> GetAllStudents()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return students;
         }
     }
 }
---
+++ stdWCF/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace stdWCF
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public string klasse { get; set; }
+        public string rum { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(string Name, string Klasse, string Rum)
+        {
+            name = Name;
+            klasse = Klasse;
+            rum = Rum;
+        }
+    }   
+}
